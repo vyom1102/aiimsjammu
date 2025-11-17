0706389789f99b674162d32dd04ab4929e7dac8c
Himanshu <aggarwakh87@gmail.com>
2025-11-17 16:42:53 +0530

    preload

--- lib/AiimsJammu/Screens/HomePage.dart
+++ lib/AiimsJammu/Screens/HomePage.dart
@@ -172,7 +172,7 @@ class _HomePageState extends State<HomePage> {
     SingletonFunctionController().mapCLustring.initMarkers();
     index = 0;
     _scrollController = ScrollController(initialScrollOffset: 140.0);
-    // requestStoragePermission();
+    requestStoragePermission();
   }
 
   Future<void> requestStoragePermission() async {
